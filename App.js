b2d8a46c847e536020eb40ddd8fd37e6c70af9c2
4rr0w <himanshu122122@gmail.com>
2024-10-04 13:57:59 +0530

    fix status bar issue

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text } from "react-native";
-import BottomBar from "./components/BottomBar";
+import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaProvider } from 'react-native-safe-area-context'; 
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 import Explore from "./screens/Explore";
 import MyCollection from "./screens/MyCollection";
+import { StatusBar } from 'expo-status-bar';
 import Settings from "./screens/Settings";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { NavigationContainer } from "@react-navigation/native";
@@ -29,81 +29,71 @@ const screenOptions = {
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={screenOptions}>
-        <Tab.Screen 
-          name="Home" 
-          component={Home}
-          options={{
-            tabBarIcon: ({ focused }) => 
-              <View style={{
-                alignItems: "center",
-                justifyContent: "center",
-              }}>  
-                <AntDesign name="home" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
-              </View>
-            }
-          }
-        />
-        <Tab.Screen 
-          name="My Collection" 
-          component={MyCollection}
-          options={{
-            tabBarIcon: ({ focused }) => 
-              <View style={{
-                alignItems: "center",
-                justifyContent: "center",
-              }}>  
-                <AntDesign name="book" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
-              </View>
-            }
-          }
-        />
-        <Tab.Screen 
-          name="Explore" 
-          component={Explore} 
-          options={{
-            tabBarIcon: ({ focused }) => 
-              <View style={{
-                alignItems: "center",
-                justifyContent: "center",
-              }}>  
-                <AntDesign name="playcircleo" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
-              </View>
-            }
-          }
-        />
-        <Tab.Screen 
-          name="Settings" 
-          component={Settings}
-          options={{
-            tabBarIcon: ({ focused }) => 
-              <View style={{
-                alignItems: "center",
-                justifyContent: "center",
-              }}>  
-                <AntDesign name="setting" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
-              </View>
-            }
-          }
-        />
-        <Tab.Screen 
-          name="Profile" 
-          component={Profile}
-          options={{
-            tabBarIcon: ({ focused }) => 
-              <View style={{
-                alignItems: "center",
-                justifyContent: "center",
-              }}>  
-                <AntDesign name="user" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
-              </View>
-            }
-          }
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <StatusBar hidden />
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen 
+            name="Home" 
+            component={Home}
+            options={{
+              tabBarIcon: ({ focused }) => 
+                <View style={styles.iconContainer}>  
+                  <AntDesign name="home" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
+                </View>
+            }}
+          />
+          <Tab.Screen 
+            name="My Collection" 
+            component={MyCollection}
+            options={{
+              tabBarIcon: ({ focused }) => 
+                <View style={styles.iconContainer}>  
+                  <AntDesign name="book" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
+                </View>
+            }}
+          />
+          <Tab.Screen 
+            name="Explore" 
+            component={Explore} 
+            options={{
+              tabBarIcon: ({ focused }) => 
+                <View style={styles.iconContainer}>  
+                  <AntDesign name="playcircleo" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
+                </View>
+            }}
+          />
+          <Tab.Screen 
+            name="Settings" 
+            component={Settings}
+            options={{
+              tabBarIcon: ({ focused }) => 
+                <View style={styles.iconContainer}>  
+                  <AntDesign name="setting" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
+                </View>
+            }}
+          />
+          <Tab.Screen 
+            name="Profile" 
+            component={Profile}
+            options={{
+              tabBarIcon: ({ focused }) => 
+                <View style={styles.iconContainer}>  
+                  <AntDesign name="user" size={28} style={{ color: focused ? "#faff00" : "#fff" }} />
+                </View>
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default App;
